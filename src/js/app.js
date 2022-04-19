b112a018996d6666a2a27d1148a3e37a9ea4d2fc
Mitchell Quinn <Mitchell_Quinn@McKinsey.com>
2022-04-19 16:27:25 -0400

    added jqeury

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+$( document ).ready(
+
+
+    function() {
+
+        $("#userProfile").animate({
+            opacity: 0.25,
+            left: "+=50",
+            height: "toggle"
+        }, 5000, function() {
+            // Animation complete.
+        });
+
+    }
+
+
+
+);
